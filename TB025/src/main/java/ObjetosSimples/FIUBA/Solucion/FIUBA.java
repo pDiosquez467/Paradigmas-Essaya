19eb67bfa60350253566a6e68194cbafe052429d
pdiosquez467 <pablodiosquez467@gmail.com>
2025-09-02 16:12:02 -0300

    Agrego implementación naive de la clase 'FIUBA'

--- TB025/src/main/java/ObjetosSimples/FIUBA/Solucion/FIUBA.java
+++ TB025/src/main/java/ObjetosSimples/FIUBA/Solucion/FIUBA.java
@@ -4,45 +4,48 @@ import java.util.*;
 
 public class FIUBA {
 
-    private static List<Carrera> carreras;
+    private static final List<Carrera> carreras = new ArrayList<>();
+    private static final HashMap<Alumno, List<Carrera>> alumnos = new HashMap<>();
 
-    private static HashMap<Alumno, List<Carrera>> alumnos;
-
-    public void agregarCarrera(Carrera carrera) throws Exception {
+    public static void agregarCarrera(Carrera carrera) throws Exception {
         if (existeCarrera(carrera)) throw new Exception("Esta carrera ya existe en la Facultad!");
         carreras.add(carrera);
     }
 
-    public boolean existeCarrera(Carrera carrera) {
+    public static boolean existeCarrera(Carrera carrera) {
         return existeEn(carrera, Collections.singleton(carreras));
     }
 
-    public void agregarAlumno(Alumno alumno) throws Exception {
+    public static void agregarAlumno(Alumno alumno) throws Exception {
         if (existeAlumno(alumno)) throw new Exception("Este alumno ya existe en la Facultad!");
         alumnos.put(alumno, new ArrayList<>());
     }
 
-    public boolean existeAlumno(Alumno alumno) {
+    public static boolean existeAlumno(Alumno alumno) {
         return existeEn(alumno, Collections.singleton(alumnos.keySet()));
     }
 
-    private Boolean existeEn(Object obj, Collection<Object> coleccion) {
+    private static boolean existeEn(Object obj, Collection<Object> coleccion) {
         for (Object object: coleccion) {
             if (object.equals(obj)) return true;
         }
         return false;
     }
 
-    public void inscribirseA(Alumno alumno, Carrera carrera) throws Exception {
+    public static void agregarMateriaACarrera(Materia materia, Carrera carrera) throws Exception {
+        carrera.agregarMateriaAPlanDeEstudios(materia);
+    }
+
+    public static void inscribirseA(Alumno alumno, Carrera carrera) throws Exception {
         alumnos.get(alumno).add(carrera);
         alumno.inscribirseACarrera(carrera);
     }
 
-    public void aprobarMateria(Alumno alumno, Materia materia) throws Exception{
+    public static void aprobarMateria(Alumno alumno, Materia materia) throws Exception{
         alumno.marcarMateriaComoAprobada(materia);
     }
 
-    public String consultarEstadoAcademico(Alumno alumno) {
+    public static String consultarEstadoAcademico(Alumno alumno) {
         return alumno.verEstadoDeCarreras();
     }
 
