f6d5a601f921da59c1e4fdd9916e161bffb0c8e6
pdiosquez467 <pablodiosquez467@gmail.com>
2025-09-02 10:22:33 -0300

    Agrego estructura base de la clase 'Materia'

--- TB025/src/main/java/ObjetosSimples/FIUBA/Solucion/Materia.java
+++ TB025/src/main/java/ObjetosSimples/FIUBA/Solucion/Materia.java
@@ -1,4 +1,39 @@
 package main.java.ObjetosSimples.FIUBA.Solucion;
 
+import main.java.ObjetosSimples.FIUBA.Solucion.utils.TipoMateria;
+
 public class Materia {
+    private final String codigo;
+    private final String nombre;
+    private int cantidadDeCreditos;
+    private TipoMateria tipo;
+
+    public Materia(String codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public int getCantidadDeCreditos() {
+        return cantidadDeCreditos;
+    }
+
+    public TipoMateria getTipo() {
+        return tipo;
+    }
+
+    public void setCantidadDeCreditos(int cantidadDeCreditos) {
+        this.cantidadDeCreditos = cantidadDeCreditos;
+    }
+
+    public void setTipo(TipoMateria tipo) {
+        this.tipo = tipo;
+    }
 }
