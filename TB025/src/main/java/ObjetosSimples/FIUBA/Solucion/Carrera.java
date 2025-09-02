6bf1d0f183d2457c0c572b0d7ab8e6ac50fd1d62
pdiosquez467 <pablodiosquez467@gmail.com>
2025-09-02 10:30:23 -0300

    Agrego estructura base de la clase 'Carrera'

--- TB025/src/main/java/ObjetosSimples/FIUBA/Solucion/Carrera.java
+++ TB025/src/main/java/ObjetosSimples/FIUBA/Solucion/Carrera.java
@@ -1,4 +1,34 @@
 package main.java.ObjetosSimples.FIUBA.Solucion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Carrera {
+    private final String codigo;
+    private final String nombre;
+    private final List<Materia> materias;
+    private final int cantidadMinimaCreditos;
+
+    public Carrera(String codigo, String nombre, int cantidadMinimaCreditos) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.materias = new ArrayList<>();
+        this.cantidadMinimaCreditos = cantidadMinimaCreditos;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public List<Materia> getMaterias() {
+        return materias;
+    }
+
+    public int getCantidadMinimaCreditos() {
+        return cantidadMinimaCreditos;
+    }
 }
