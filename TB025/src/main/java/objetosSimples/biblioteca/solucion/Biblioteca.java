7bb8eec4bc7b356f8b0ce608bb8d1e9df3707020
pdiosquez467 <pablodiosquez467@gmail.com>
2025-09-29 10:43:19 -0300

    Agrego métodos parciales que requieren ser completados y revisados.

--- TB025/src/main/java/objetosSimples/biblioteca/solucion/Biblioteca.java
+++ TB025/src/main/java/objetosSimples/biblioteca/solucion/Biblioteca.java
@@ -3,6 +3,8 @@ package main.java.objetosSimples.biblioteca.solucion;
 import main.java.objetosSimples.validaciones.Validaciones;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class Biblioteca {
 
@@ -65,11 +67,57 @@ public class Biblioteca {
 
     // === PRÉSTAMOS ===
 
+    public void prestarLibro(LibroEnBiblioteca libroEnBiblioteca, Usuario usuario) {
+        // IDEA: Podríamos tener una clase 'Préstamo' que gestione préstamos y devoluciones, fechas y demás.
 
+        if (!contieneLibro(libroEnBiblioteca)) {
+            throw new RuntimeException();
+        }
 
+        if (!contieneUsuario(usuario)) {
+            throw new RuntimeException();
+        }
 
+        libroEnBiblioteca.gestionarPrestamo();
+        usuario.agregarLibro(libroEnBiblioteca.getLibro());
+    }
 
+    public void devolverLibro(LibroEnBiblioteca libroEnBiblioteca, Usuario usuario) {
 
+    }
 
+    // === CONSULTAS ===
 
+    public List<Libro> librosPrestados(Usuario usuario) {
+        Validaciones.validarNotNull(usuario, "Usuario");
+        if (!contieneUsuario(usuario)) {
+            throw new RuntimeException("El usuario no pertenece a la biblioteca");
+        }
+        return usuario.getLibrosPrestados();
+    }
+
+    public int consultarStock(LibroEnBiblioteca libroEnBiblioteca) {
+        Validaciones.validarNotNull(libroEnBiblioteca, "Libro");
+        if (!contieneLibro(libroEnBiblioteca)) {
+            throw new RuntimeException("El libro no pertenece a la biblioteca");
+        }
+        return libros.get(libroEnBiblioteca.isbn()).getCantidadDeCopias();
+    }
+
+    public int consultarStockPorTitulo(String titulo) {
+        Validaciones.validarNotNull(titulo, "Libro");
+        return consultarStock(libroEnBiblioteca -> libroEnBiblioteca.getLibro().titulo().equals(titulo));
+    }
+
+    public int consultarStockPorAutor(String autor) {
+        Validaciones.validarNotNull(autor, "Libro");
+        return consultarStock(libroEnBiblioteca -> libroEnBiblioteca.getLibro().autores().contains(autor));
+    }
+
+    private int consultarStock(Predicate<LibroEnBiblioteca> criterio) {
+        return libros.values().stream()
+                .filter(criterio)
+                .mapToInt(LibroEnBiblioteca::getCantidadDeCopias)
+                .sum();
+    }
 }
