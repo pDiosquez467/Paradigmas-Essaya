499eca5f3ba51fa196b14cd053a3bc1e8998dc48
pdiosquez467 <pablodiosquez467@gmail.com>
2025-09-29 09:54:07 -0300

    Agrego impl del método 'equals & hashCode' de la clase 'Libro'

--- TB025/src/main/java/objetosSimples/biblioteca/solucion/Libro.java
+++ TB025/src/main/java/objetosSimples/biblioteca/solucion/Libro.java
@@ -4,6 +4,7 @@ import main.java.objetosSimples.validaciones.Validaciones;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 
 public record Libro(String isbn, String titulo, LocalDate fechaDePublicacion, List<String> autores) {
     public Libro {
@@ -14,5 +15,16 @@ public record Libro(String isbn, String titulo, LocalDate fechaDePublicacion, Li
         Validaciones.validarNotNull(fechaDePublicacion, "Fecha de publicación");
         Validaciones.validarNotNull(autores, "Autores");
 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Libro libro)) return false;
+        return Objects.equals(isbn, libro.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(isbn);
     }
 }
